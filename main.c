592a961c2236689aa7195fb46476ee59b7e04e78
Vien <vinybf@hotmail.com>
2018-10-05 20:42:58 -0300

    added const modifier to function's arguments

--- main.c
+++ main.c
@@ -3,37 +3,39 @@
 
 
 
-void mdc_estendido(mpz_t g, mpz_t x, mpz_t y, mpz_t a, mpz_t b)
+void mdc_estendido(mpz_t g, mpz_t x, mpz_t y, const mpz_t a, const mpz_t b)
 {
-    mpz_t x1, y1, u ,v, q, r, m, n, aux1, aux2;
-    mpz_inits(x1, y1, u, v, q, r, m, n, aux1, aux2, NULL);
+    mpz_t x1, y1, u ,v, q, r, m, n, aux1, aux2, a1, b1;
+    mpz_inits(x1, y1, u, v, q, r, m, n, aux1, aux2, a1, b1, NULL);
     mpz_set_ui(x1, 0);
     mpz_set_ui(y1, 1);
     mpz_set_ui(u, 1);
     mpz_set_ui(v, 0);
+    mpz_set(a1, a);
+    mpz_set(b1, b);
 
-    while(mpz_cmp_ui(a, 0) != 0)
+    while(mpz_cmp_ui(a1, 0) != 0)
     {
-        mpz_tdiv_qr(q, r, b, a);
+        mpz_tdiv_qr(q, r, b1, a1);
         mpz_mul(aux1, u, q);
         mpz_sub(aux2, x1, aux1);
         mpz_set(m, aux2);
         mpz_mul(aux1, v, q);
         mpz_sub(aux2, y1, aux1);
         mpz_set(n, aux2);
-        mpz_set(b, a);
-        mpz_set(a, r);
+        mpz_set(b1, a1);
+        mpz_set(a1, r);
         mpz_set(x1, u);
         mpz_set(y1, v);
         mpz_set(u, m);
         mpz_set(v, n);
     }
-    mpz_set(g, b);
+    mpz_set(g, b1);
     mpz_set(x, x1);
     mpz_set(y, y1);
 }
 
-int inverso_modular(mpz_t r, mpz_t a, mpz_t n)
+int inverso_modular(mpz_t r, const mpz_t a, const mpz_t n)
 {
     mpz_t g, x, y;
     mpz_inits(g, x, y , NULL);
@@ -49,19 +51,21 @@ int inverso_modular(mpz_t r, mpz_t a, mpz_t n)
 
 
 
-void mdc(mpz_t g, mpz_t a,  mpz_t b)
+void mdc(mpz_t g, const mpz_t a, const mpz_t b)
 {
-    mpz_t q, r;
+    mpz_t q, r, a1, b1;
     mpz_inits(q, r, NULL);
+    mpz_set(a1, a);
+    mpz_set(b1, b);
     
 
-    while(mpz_cmp_ui(b, 0) != 0)
+    while(mpz_cmp_ui(b1, 0) != 0)
     {
-        mpz_tdiv_r(r, a, b);
-        mpz_set(a, b);
-        mpz_set(b, r);
+        mpz_tdiv_r(r, a1, b1);
+        mpz_set(a1, b1);
+        mpz_set(b1, r);
     }
-    mpz_set(g, a);
+    mpz_set(g, a1);
 }
 
 
@@ -70,13 +74,14 @@ void main()
     mpz_t a, b, x, y, g, r;
     mpz_inits(a, b, g, x, y, r, NULL);
     gmp_scanf("%Zd %Zd", a, b);
-    mdc(g, a, b);
-    gmp_printf("mdc = %Zd\n", g);
+    mdc_estendido(g, x, y, a, b);
+    gmp_printf("(%Zd)*(%Zd) + (%Zd)*(%Zd) = mdc(%Zd,%Zd) = %Zd\n",
+                                                     x, a, y, b, a, b, g);
     if(inverso_modular(r, a, b))
     {
-        gmp_printf("inverso modular = %Zd\n", r);
+        gmp_printf("Inverso modular = %Zd\n", r);
 
     }
     else
-        gmp_printf("nao existe inverso modular\n");
+        gmp_printf("Nao existe inverso modular\n");
 }
